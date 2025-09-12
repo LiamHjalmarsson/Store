ca322920ea5c7be8c9aafe996bdb723a3183774d
Liam Hjalmarsson <liam.hjalmarsson@hotmail.com>
2025-09-12 12:26:11 +0200

    Category

--- api/src/models/category/categoryModal.ts
+++ api/src/models/category/categoryModal.ts
@@ -5,7 +5,10 @@ import { Category } from "../../types/category";
 export async function createCategory(): Promise<Category> {
 	await ensureCategoryTable();
 
-	const result = await query<Category>("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *", []);
+	const result = await query<Category>(
+		"INSERT INTO categories (title, description, image, create_at) VALUES ($1, $2, $3, $4) RETURNING *",
+		[]
+	);
 
 	return result.rows[0];
 }
