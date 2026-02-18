695bf03aabc16232987da8a7809c6383a87e7422
Liam Hjalmarsson <liam.hjalmarsson@hotmail.com>
2026-02-18 15:43:02 +0100

    users

--- client/src/pages/admin/users/Users.vue
+++ client/src/pages/admin/users/Users.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { getUsers, type User } from "../../../api/user/user";
+import Card from "../../../components/ui/card.vue";
 
 const users = ref<User[]>([]);
 
@@ -14,48 +15,42 @@ onMounted(async () => {
 <template>
 	<div class="container mx-auto">
 		<div class="grid md:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-6">
-			<div v-for="user in users" :key="user.id" class="p-1.5 space-y-8 shadow rounded-4xl">
-				<div class="h-32 w-full bg-black/80 relative rounded-3xl">
+			<Card v-for="user in users" :key="user.id" class="p-1.5 space-y-8 shadow rounded-4xl">
+				<template #icon>
 					<div class="absolute left-5 -bottom-4 w-16 h-16 rounded-full">
 						<div class="relative">
 							<img :src="user.avatar" class="rounded-full h-full w-full" />
 							<div class="h-3 w-3 bg-green-400 rounded-full absolute right-0.5 bottom-1.5" />
 						</div>
 					</div>
-				</div>
-				<div class="px-2 space-y-6">
-					<div class="flex justify-between items-center">
-						<div class="">
-							<span class="block font-medium">
-								{{ user.firstname }}
-								{{ user.lastname }}
-							</span>
-							<span class="block text-xs">{{ user.email }}</span>
-						</div>
-						<div class="w-6 h-6 border/5 rounded-full flex justify-center items-center bg-black/5"></div>
-					</div>
+				</template>
 
-					<div class="grid grid-cols-3">
-						<div class="flex flex-col items-center">
-							<div class="font-bold">10+</div>
-							<div class="text-xs text-black/60">Reviews</div>
-						</div>
-						<div class="flex flex-col items-center">
-							<div class="font-bold">50+</div>
-							<div class="text-xs text-black/60">Orders</div>
-						</div>
-						<div class="flex flex-col items-center">
-							<div class="font-bold">5000</div>
-							<div class="text-xs text-black/60">Spent</div>
-						</div>
+				<template #heading>
+					<div>
+						<span class="block font-medium">
+							{{ user.firstname }}
+							{{ user.lastname }}
+						</span>
+						<span class="block text-xs">{{ user.email }}</span>
 					</div>
+					<div class="w-6 h-6 border/5 rounded-full flex justify-center items-center bg-black/5"></div>
+				</template>
 
-					<button
-						class="w-full py-3 px-6 bg-black text-white rounded-full cursor-pointer duration-150 hover:bg-black/80">
-						See more
-					</button>
-				</div>
-			</div>
+				<template #stats>
+					<div class="flex flex-col items-center">
+						<div class="font-bold">10+</div>
+						<div class="text-xs text-black/60">Reviews</div>
+					</div>
+					<div class="flex flex-col items-center">
+						<div class="font-bold">50+</div>
+						<div class="text-xs text-black/60">Orders</div>
+					</div>
+					<div class="flex flex-col items-center">
+						<div class="font-bold">5000</div>
+						<div class="text-xs text-black/60">Spent</div>
+					</div>
+				</template>
+			</Card>
 		</div>
 	</div>
 </template>
