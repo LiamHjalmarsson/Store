cea5421941a4adf3485dde4dafc701f6d642db81
Liam Hjalmarsson <liam.hjalmarsson@hotmail.com>
2026-02-26 16:22:44 +0100

    Creator NotFound

--- src/modules/category/service/categoryService.ts
+++ src/modules/category/service/categoryService.ts
@@ -11,13 +11,8 @@ export const getAllCategoriesService = async () => {
 	return await findAllCategories();
 };
 
-export const createCategoryService = async (data: CreateCategoryPayload) => {
-	return await createNewCategory({
-		title: data.title ?? "",
-		description: data.description,
-		image: data.image,
-		is_featured: data.is_featured ?? false,
-	});
+export const createCategoryService = async (payload: CreateCategoryPayload) => {
+	return await createNewCategory(payload);
 };
 
 export const getCategoryService = async (categoryId: number) => {
--- src/modules/creator/controller/creatorController.ts
+++ src/modules/creator/controller/creatorController.ts
@@ -8,114 +8,91 @@ import {
 } from "../service/creatorService.js";
 import { AuthenticatedRequest } from "../../../shared/middlewares/authenicated.js";
 import { CreateCreatorPayload } from "../types/creator.js";
+import { NotFoundError } from "../../../shared/errors/notFound.js";
 
 export const getAllCreators = async (req: Request, res: Response) => {
-	try {
-		const creators = await getAllCreatorsService();
+	const creators = await getAllCreatorsService();
 
-		res.json({ creators });
-	} catch (err) {
-		res.status(500).json({ message: "Server error" });
-	}
+	res.json({ creators });
 };
 
 export const createCreator = async (req: AuthenticatedRequest, res: Response) => {
-	try {
-		const userId = Number(req.user?.id);
+	const userId = Number(req.user?.id);
 
-		const payload: CreateCreatorPayload = {
-			user_id: userId,
-			...req.body,
-		};
+	const payload: CreateCreatorPayload = {
+		user_id: userId,
+		...req.body,
+	};
 
-		const creator = await createCreatorService(payload);
+	const creator = await createCreatorService(payload);
 
-		res.json({ creator });
-	} catch (error) {
-		res.status(404).json({ message: "Server error", error });
-	}
+	res.json({ creator });
 };
 
 export const getCreator = async (req: Request, res: Response) => {
-	try {
-		const id = Number(req.params.id);
+	const id = Number(req.params.id);
 
-		const creator = await getCreatorService(id);
+	const creator = await getCreatorService(id);
 
-		if (!creator) {
-			return res.status(404).json({ message: "Creator not found" });
-		}
-
-		res.json({ creator });
-	} catch (error) {
-		res.status(500).json({ message: "Server error", error });
+	if (!creator) {
+		throw new NotFoundError("Creator not found");
 	}
+
+	res.json({ creator });
 };
 
 export const updateCreatorProfile = async (req: AuthenticatedRequest, res: Response) => {
-	try {
-		const userId = Number(req.user?.id);
+	const userId = Number(req.user?.id);
 
-		if (!req.body) {
-			return res.status(400).json({ message: "No fields provided to update" });
-		}
-
-		const updated = await updateCreatorService(userId, req.body);
-
-		if (!updated) {
-			return res.status(404).json({ message: "Creator not found" });
-		}
-
-		res.json({ creator: updated });
-	} catch (error) {
-		res.status(404).json({ message: "Server error", error });
+	if (!req.body) {
+		return res.status(400).json({ message: "No fields provided to update" });
 	}
+
+	const updated = await updateCreatorService(userId, req.body);
+
+	if (!updated) {
+		throw new NotFoundError("Creator not found");
+	}
+
+	res.json({ creator: updated });
 };
 
 export const updateCreator = async (req: Request, res: Response) => {
 	const userId = Number(req.params.id);
 
-	try {
-		if (!req.body) {
-			return res.status(400).json({ message: "No fields provided to update" });
-		}
-
-		const updated = await updateCreatorService(userId, req.body);
-
-		if (!updated) {
-			return res.status(404).json({ message: "Creator not found" });
-		}
-
-		res.json({ creator: updated });
-	} catch (error) {
-		res.status(404).json({ message: "Server error", error });
+	if (!req.body) {
+		return res.status(400).json({ message: "No fields provided to update" });
 	}
+
+	const updated = await updateCreatorService(userId, req.body);
+
+	if (!updated) {
+		throw new NotFoundError("Creator not found");
+	}
+
+	res.json({ creator: updated });
 };
 
 export const deleteCreatorProfile = async (req: AuthenticatedRequest, res: Response) => {
-	try {
-		const userId = Number(req.params.id);
+	const userId = Number(req.params.id);
 
-		const deleted = await deleteCreatorService(userId);
+	const deleted = await deleteCreatorService(userId);
 
-		if (!deleted) return res.status(404).json({ message: "Creator profile not found" });
-
-		res.json({ message: "Creator profile deleted successfully" });
-	} catch (error) {
-		res.status(404).json({ message: "Server error", error });
+	if (!deleted) {
+		throw new NotFoundError("Creator profile not found");
 	}
+
+	res.json({ message: "Creator profile deleted successfully" });
 };
 
 export const deleteCreator = async (req: AuthenticatedRequest, res: Response) => {
-	try {
-		const userId = Number(req.user?.id);
+	const userId = Number(req.user?.id);
 
-		const deleted = await deleteCreatorService(userId);
+	const deleted = await deleteCreatorService(userId);
 
-		if (!deleted) return res.status(404).json({ message: "Creator profile not found" });
-
-		res.json({ message: "Creator profile deleted successfully" });
-	} catch (error) {
-		res.status(404).json({ message: "Server error", error });
+	if (!deleted) {
+		throw new NotFoundError("Creator profile not found");
 	}
+
+	res.json({ message: "Creator profile deleted successfully" });
 };
