55a0ad442ac04645f6c3a09e69533360256b7bc6
Liam Hjalmarsson <liam.hjalmarsson@hotmail.com>
2026-02-26 16:36:40 +0100

    user

--- src/modules/subcategory/controller/subcategoryController.ts
+++ src/modules/subcategory/controller/subcategoryController.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from "express";
 import { getAllSubcategoriesService } from "../service/subcategoryService.js";
 
-export const getAllSubcategories = async (req: Request, res: Response) => {
-	try {
-		const subcategories = await getAllSubcategoriesService();
+export const getAllSubcategories = async (_: Request, res: Response) => {
+	const subcategories = await getAllSubcategoriesService();
 
-		res.json({ subcategories });
-	} catch (error) {
-		res.status(500).json({ message: "Server error", error });
-	}
+	res.json({ subcategories });
 };
