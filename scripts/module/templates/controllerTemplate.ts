ca3c107c4b10cfa3db172254419dfa02622e75d9
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2026-03-20 22:28:06 +0100

    module templatecontroller

--- scripts/module/templates/controllerTemplate.ts
+++ scripts/module/templates/controllerTemplate.ts
@@ -1,3 +1,27 @@
+import { capitalize } from "../utils/capitalize";
+import { pluralize } from "../utils/pluralize";
+
 export function controllerTemplate(name: string) {
-	return ``;
+	const cap = capitalize(name);
+
+	return `
+		import { Request, Response } from "express";
+		import { sendError, sendSuccess } from "../../../shared/utils/respond.js";
+
+		export const getAll${pluralize(cap)}Controller = async (_: Request, res: Response) => {
+		};
+
+		export const create${pluralize(cap)}Controller = async (req: Request, res: Response) => {
+		};
+
+		export const get${pluralize(cap)}Controller = async (req: Request, res: Response) => {
+		};
+
+		export const update${pluralize(cap)}Controller = async (req: Request, res: Response) => {
+		};
+
+		export const delete${pluralize(cap)}Controller = async (req: Request, res: Response) => {
+		};
+
+	`;
 }
