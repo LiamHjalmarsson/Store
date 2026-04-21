ff8d38a50b4a16c187a468cf4f800b2cd5606927
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2026-04-21 21:33:25 +0200

    Ach

--- src/modules/achievement/constants/achievementMessages.ts
+++ src/modules/achievement/constants/achievementMessages.ts
@@ -1,10 +1,4 @@
 export const ACHIEVEMENT_MESSAGES = {
-	RETRIEVED_ALL: "Achievements retrieved successfully",
-	CREATED: "Achievement created successfully",
-	UPDATED: "Achievement updated successfully",
-	DELETED: "Achievement deleted successfully",
-	USER_RETRIEVED: "User achievements retrieved successfully",
 	AWARDED: "Achievement awarded successfully",
 	ALREADY_AWARDED: "Achievement already awarded",
-	INVALID_ID: "Invalid achievement ID",
 } as const;
