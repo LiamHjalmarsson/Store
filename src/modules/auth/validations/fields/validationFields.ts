66a4879a1de79149e556cf717e2802260715cc72
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2026-04-21 21:57:45 +0200

    Auth

--- src/modules/auth/validations/fields/validationFields.ts
+++ src/modules/auth/validations/fields/validationFields.ts
@@ -1,13 +1,14 @@
 import { body } from "express-validator";
 import emailUnique from "../../../../shared/validations/rules/emailUnique.js";
 import usernameUnique from "../../../../shared/validations/rules/usernameUnique.js";
+import { VALIDATION_MESSAGES } from "../../../../shared/constants/validationMessages.js";
 
 export function emailField() {
 	return body("email")
 		.trim()
 		.toLowerCase()
 		.notEmpty()
-		.withMessage("Email is required")
+		.withMessage(VALIDATION_MESSAGES.REQUIRED("Email"))
 		.isEmail()
 		.withMessage("Email must be valid");
 }
@@ -17,13 +18,13 @@ export function uniqueEmailField() {
 }
 
 export function loginPasswordField() {
-	return body("password").notEmpty().withMessage("Password is required");
+	return body("password").notEmpty().withMessage(VALIDATION_MESSAGES.REQUIRED("Password"));
 }
 
 export function registerPasswordField() {
 	return body("password")
 		.notEmpty()
-		.withMessage("Password is required")
+		.withMessage(VALIDATION_MESSAGES.REQUIRED("Password"))
 		.isLength({ min: 6 })
 		.withMessage("Password must be at least 6 characters long");
 }
@@ -32,7 +33,7 @@ export function usernameField() {
 	return body("username")
 		.trim()
 		.notEmpty()
-		.withMessage("Username is required")
+		.withMessage(VALIDATION_MESSAGES.REQUIRED("Username"))
 		.isLength({ min: 3, max: 30 })
 		.withMessage("Username must be between 3 and 30 characters")
 		.custom(usernameUnique);
